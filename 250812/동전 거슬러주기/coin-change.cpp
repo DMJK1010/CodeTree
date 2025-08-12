ad2634e770ecc61cdc9210083c43d0fa3e1213d9
Junha <141795109+DMJK1010@users.noreply.github.com>
2025-08-12 19:30:39 +0900

    Title: 동전 거슬러주기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250812/동전 거슬러주기/coin-change.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, M;
+int coin[100];
+
+int main() {
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++) {
+        cin >> coin[i];
+    }
+
+    vector<int> D(M + 1, INT_MAX); 
+    D[0] = 0;
+    
+    for (int i = 1; i <= M; i++) {
+        for (int j = 0; j < N; j++) {
+            if (i >= coin[j]) {
+                if (D[i - coin[j]] != INT_MAX) {
+                    D[i] = min(D[i], D[i - coin[j]] + 1);
+                }
+            }
+        }
+    }
+
+    int ans = D[M];
+
+    if (ans == INT_MAX) {
+        cout << -1 << endl;
+    } else {
+        cout << ans << endl;
+    }
+
+    return 0;
+}
